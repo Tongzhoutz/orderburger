47277a8394bc59fe12716c28472d4dcc8317cb0b
tongzhoutz <tzhou11@jhu.edu>
2023-01-13 10:34:39 -0500

    refactor: 💡 change app contents

--- src/App.js
+++ src/App.js
@@ -6,46 +6,46 @@ import SearchBar from './components/SearchBar/SearchBar';
 const MEALS_DATA = [
   {
     id: '1',
-    title: '汉堡包',
-    desc: '百分百纯牛肉配搭爽脆酸瓜洋葱粒与美味番茄酱经典滋味让你无法抵挡！',
-    price: 12,
+    title: 'Croissanwich Meal Small',
+    desc: 'Sizzling sausage, fluffy eggs, and melted American cheese.',
+    price: 6.39,
     img: '/img/meals/1.png'
   },
   {
     id: '2',
-    title: '双层吉士汉堡',
-    desc: '百分百纯牛肉与双层香软芝，加上松软面包及美味酱料，诱惑无人能挡！',
+    title: 'Croissanwich Meal Small',
+    desc: 'Black Forest ham, sizzling sausage, crispy bacon, fluffy eggsheese.',
     price: 20,
     img: '/img/meals/2.png'
   },
   {
     id: '3',
-    title: '巨无霸',
-    desc: '两块百分百纯牛肉，搭配生菜、洋葱等新鲜食材，口感丰富，极致美味！',
+    title: 'Bacon, Sausage & Ham Fully Croissanwich',
+    desc: 'Black Forest ham, sizzling sausage, crispy bacon, fluffy eggs',
     price: 24,
     img: '/img/meals/3.png'
   }, {
     id: '4',
-    title: '麦辣鸡腿汉堡',
-    desc: '金黄脆辣的外皮，鲜嫩幼滑的鸡腿肉，多重滋味，一次打动您挑剔的味蕾！',
+    title: 'Fully Loaded Croissanwich',
+    desc: 'Black Forest ham, sizzling sausage, crispy bacon, fluffy eggs',
     price: 21,
     img: '/img/meals/4.png'
   }, {
     id: '5',
-    title: '板烧鸡腿堡',
-    desc: '原块去骨鸡排嫩滑多汁，与翠绿新鲜的生菜和香浓烧鸡酱搭配，口感丰富！',
+    title: 'Sausage Biscuit',
+    desc: 'Sizzling sausage, fluffy eggs, and melted American cheese ',
     price: 22,
     img: '/img/meals/5.png'
   }, {
     id: '6',
-    title: '麦香鸡',
-    desc: '清脆爽口的生菜，金黄酥脆的鸡肉。营养配搭，好滋味的健康选择！',
+    title: 'Sausage Croissanwich',
+    desc: 'Two sizzling sausage patties, fluffy eggs and melted American cheese',
     price: 14,
     img: '/img/meals/6.png'
   }, {
     id: '7',
-    title: '吉士汉堡包',
-    desc: '百分百纯牛肉与香软芝士融为一体配合美味番茄醬丰富口感一咬即刻涌现！',
+    title: 'Egg-Normous Burrito',
+    desc: 'Sizzling sausage, crispy bacon, fluffy eggs, melted American cheese',
     price: 12,
     img: '/img/meals/7.png'
   }
